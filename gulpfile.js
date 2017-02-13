fa40816a67b49f977a6af4b56cbb58e69a1d6039
JulianaTennett <tennett.j@gmail.com>
2017-02-12 22:55:17 -0500

    add in gulp and sass

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+    imagemin = require('gulp-imagemin'),
+    cache = require('gulp-cache'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync');
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+       baseDir: "./"
+    }
+  });
+});
+
+gulp.task('bs-reload', function () {
+  browserSync.reload();
+});
+
+gulp.task('images', function(){
+  gulp.src('src/img/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/images/'));
+});
+
+gulp.task('styles', function(){
+  gulp.src(['scss/**/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest('dist/styles/'))
+    .pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('default', ['browser-sync'], function(){
+  gulp.watch("scss/**/*.scss", ['styles']);
+  gulp.watch("*.html", ['bs-reload']);
+});
